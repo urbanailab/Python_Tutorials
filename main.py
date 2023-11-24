7dc8acc3655898b27fa73ee86930b43c07bc23e0
caojh <jcao1231@outlook.com>
2023-11-24 18:22:16 -0500

    main change

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-# 这是一个示例 Python 脚本。
+# main.py
 
-# 按 ⌃R 执行或将其替换为您的代码。
-# 按 双击 ⇧ 在所有地方搜索类、文件、工具窗口、操作和设置。
+from nbconvert import PythonExporter
+import os
 
+def run_notebook(notebook_path):
+    with open(notebook_path, 'r', encoding='utf-8') as f:
+        # Use nbconvert to convert the notebook to Python code
+        exporter = PythonExporter()
+        python_code, _ = exporter.from_file(f)
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 ⌘F8 切换断点。
+    # Execute the Python code in the current namespace
+    exec(python_code, globals())
 
+if __name__ == "__main__":
+    # Run different notebooks by passing the paths
+    notebook1_path = "GoogleMapAPI.ipynb"
+    run_notebook(notebook1_path)
 
-# 按装订区域中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
